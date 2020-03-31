b7c37037ec69ee17ea433ef0945c0cc84529c924
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2020-03-31 14:19:56 +0100

    Added basic server and client

---
+++ client/client.py
@@ -0,0 +1,15 @@
+'''
+The main client script to send requests to the server
+'''
+
+import socket
+
+HOST = ""
+PORT = ""
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
---
+++ server/server.py
@@ -0,0 +1,20 @@
+'''
+The main server script which will recieve and handel requests
+'''
+
+import socket
+
+HOST = ""
+PORT = "9876"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
